5872a72e1ad66163a15fd75c714fa2d9457ad198
Joseph Hunt <josephhunt@Josephs-MacBook-Pro.local>
2017-12-13 13:49:37 -0600

    add prompt in 3-shutdown-all.sh to prevent inadvertent shutdowns

--- host_factory/setup_summon.sh
+++ host_factory/setup_summon.sh
@@ -3,6 +3,4 @@
 sudo chmod a+w /etc/hosts
 sudo echo $(netstat -rn | awk '/^0.0.0.0/ {print $2}') "conjur" >> /etc/hosts
 sudo apt-get install jq
-curl -LO https://github.com/conjurinc/summon-conjur/releases/download/v0.2.0/summon-conjur_v0.2.0_linux-amd64.tar.gz
-tar xvf summon-conjur_v0.2.0_linux-amd64.tar.gz
-sudo mv summon-conjur /usr/local/bin
+curl -sSL https://raw.githubusercontent.com/cyberark/summon/master/install.sh | bash
