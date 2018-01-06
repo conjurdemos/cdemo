6775ff86c320c37faa4550d8504da031a76db286
Joseph Hunt <josephhunt@Josephs-MacBook-Pro.local>
2018-01-06 18:42:55 +0000

    added bastion demo, improved haproxy update and seed file transfer

--- ssh_ansible/2_ssh_user_to_host.sh
+++ ssh_ansible/2_ssh_user_to_host.sh
@@ -7,4 +7,4 @@ USER=$1
 CNAME=$2
 printf "\n\nUser %s attempting to ssh from CLI container to container %s:\n\n" $USER $CNAME 
 docker exec $CNAME service nscd restart > /dev/null
-docker-compose exec cli ssh -i /src/ssh_ansible/id_$USER $USER@$CNAME
+docker-compose exec cli ssh -o StrictHostKeyChecking=no -i /src/ssh_ansible/id_$USER $USER@$CNAME
