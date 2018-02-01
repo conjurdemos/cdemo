459cb140a8499f56782f6cfe12396123c1dbc63f
Benjamin Floyd <dataplex@gmail.com>
2018-02-01 16:05:05 -0600

    Updating bastion script to use top level config file and moved to scenarios

--- scenarios/bastion/0-setup-bastion.sh
+++ scenarios/bastion/0-setup-bastion.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 set -eo pipefail
 
-CONJUR_MASTER_ORGACCOUNT=dev
-CONJUR_APPLIANCE_URL=https://conjur_follower/api
-CONJUR_CONF_FILE=../etc/conjur_follower.conf
-CONJUR_CERT_FILE=../etc/conjur_follower.pem
+. ../../etc/_loadcfg.sh
+
+CONJUR_APPLIANCE_URL=https://$CONJUR_FOLLOWER_INGRESS/api
+CONJUR_CONF_FILE=../../etc/conjur_follower.conf
+CONJUR_CERT_FILE=../../etc/conjur_follower.pem
 ACCESS_POLICY_FILE=ssh-bastion.yml
 BASTION_SERVICES="outside bastion protected"
 BASTION_CONT_NAMES="bastion_server protected_vm"
@@ -21,27 +22,27 @@ main() {
 ######################
 load_policy() {
 	printf "\n-----\nLoading bastion server access policy...\n"
-	docker exec conjur_cli conjur authn login -u admin -p Cyberark1
-	docker exec conjur_cli conjur policy load --as-group=security_admin /src/bastion/$ACCESS_POLICY_FILE
+	docker exec $CLI_CONT_NAME conjur authn login -u admin -p $CONJUR_MASTER_PASSWORD
+	docker exec $CLI_CONT_NAME conjur policy load --as-group=security_admin /src/scenarios/bastion/$ACCESS_POLICY_FILE
 }
 
 ######################
 bring_up_vms() {
 	printf "\n-----\nBringing down old, then up all vm containers...\n"
 	# the outside and bastion VMs are both on the external network (netwkx)
-	docker-compose rm -svf $BASTION_SERVICES
-	docker-compose up -d $BASTION_SERVICES
+	docker-compose -f ../../docker-compose.yml rm -svf $BASTION_SERVICES
+	docker-compose -f ../../docker-compose.yml up -d $BASTION_SERVICES
 }
 
 ######################
 conjurize_vms() {
 	printf "\n-----\nConfiguring hosts for SSH & identities ...\n"
        
-	api_key=$(docker exec conjur_cli conjur host rotate_api_key --host bastion/server)
+	api_key=$(docker exec $CLI_CONT_NAME conjur host rotate_api_key --host bastion/server)
 	conjurize_container_as_host bastion_server bastion/server $api_key
 
-	hf_token=$(docker exec conjur_cli conjur hostfactory tokens create --duration-minutes=1 protected | jq -r .[].token) 
-        api_key=$(docker exec conjur_cli conjur hostfactory hosts create $hf_token protected/vm | jq -r .api_key)
+	hf_token=$(docker exec $CLI_CONT_NAME conjur hostfactory tokens create --duration-minutes=1 protected | jq -r .[].token) 
+        api_key=$(docker exec $CLI_CONT_NAME conjur hostfactory hosts create $hf_token protected/vm | jq -r .api_key)
 	conjurize_container_as_host protected_vm protected/vm $api_key
 
 }
@@ -83,11 +84,11 @@ setup_user() {
 	user=$1
 	printf "\n\n-----\nGenerating SSH keys for user %s and adding public key to Conjur...\n" $user
 	ssh-keygen -q -b 2048 -t rsa -C ${user}-ssh-demo -f id_$user -N ''
-	docker-compose exec -T cli conjur pubkeys add $user @/src/bastion/id_$user.pub
+	docker-compose exec -T cli conjur pubkeys add $user @/src/scenarios/bastion/id_$user.pub
 
   	docker exec outside_vm sudo useradd -m $user
 	docker exec outside_vm sudo su $user -c 'mkdir ~/.ssh'
-	docker exec outside_vm sudo cp /src/bastion/id_$user /home/$user/.ssh/id_rsa
+	docker exec outside_vm sudo cp /src/scenarios/bastion/id_$user /home/$user/.ssh/id_rsa
 	docker exec outside_vm sudo chown $user:$user /home/$user/.ssh/id_rsa
 	docker exec outside_vm sudo chmod 0600 /home/$user/.ssh/id_rsa
 	cat <<SSH_CONFIG | docker exec -i outside_vm sudo su $user -c 'tee ~/.ssh/config'
