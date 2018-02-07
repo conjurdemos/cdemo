9fabee68d237285fb5fe3ec29f4c87b1269920b3
Benjamin Floyd <dataplex@gmail.com>
2018-02-07 09:18:28 -0600

    Updating cluster scenario

--- scenarios/cluster/0-setup-standbys.sh
+++ scenarios/cluster/0-setup-standbys.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 set -eo pipefail
 
+. ../../etc/_loadcfg.sh
+
 CLUSTER_NAME=dev
 CLUSTER_POLICY_FILE=cluster.yml
 
-CONJUR_MASTER_CNAME=conjur1
-CONJUR_MASTER_IP="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' conjur1)"
-CONJUR_MASTER_INGRESS=conjur_master
+CONJUR_MASTER_IP="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONJUR_MASTER_CONT_NAME)"
 NUM_STATEFUL_NODES=3			# 1 master + 2 standbys
 CONT_LIST=""	# list of stateful nodes
 		# conjur version components for checking if auto-failover is supported
@@ -15,36 +15,41 @@ CONJUR_MAJOR=""
 CONJUR_MINOR=""
 CONJUR_POINT=""
 
+HEALTH_URL="https://$CONJUR_MASTER_INGRESS/health"
+
 main() {
 	start_new_standbys
 	wait_for_healthy_master
 	setup_standbys
 	wait_for_standbys
 					# start synchronous replication
-	docker exec $CONJUR_MASTER_CNAME bash -c "evoke replication sync"
+	docker exec $CONJUR_MASTER_CONT_NAME bash -c "evoke replication sync"
 	wait_for_healthy_master
 	setup_cluster_mgr
 	update_load_balancer
-	../inspect-cluster.sh
+	../../inspect-cluster.sh
 }
 
 #############################
 start_new_standbys() {
-	printf "\n-----\nBringing up new standby node(s)...\n"
+	announce_section "Bringing up new standby node(s)..."
+	tmp_pwd=$(pwd)
+	cd ../../
 	docker-compose up -d conjur2
 	docker-compose up -d conjur3
+	cd $tmp_pwd
 }
 
 #############################
 setup_standbys() {
-	printf "\n-----\nConfiguring standby nodes...\n"
+	announce_section "Configuring standby nodes..."
 
 			# generate seed file & pipe to standby for unpacking
-	docker exec $CONJUR_MASTER_CNAME evoke seed standby \
+	docker exec $CONJUR_MASTER_CONT_NAME evoke seed standby \
 		| docker exec -i conjur2 evoke unpack seed -
 	docker exec conjur2 evoke configure standby -j /src/etc/conjur.json -i $CONJUR_MASTER_IP
 
-	docker exec $CONJUR_MASTER_CNAME evoke seed standby \
+	docker exec $CONJUR_MASTER_CONT_NAME evoke seed standby \
 		| docker exec -i conjur3 evoke unpack seed -
 	docker exec conjur3 evoke configure standby -j /src/etc/conjur.json -i $CONJUR_MASTER_IP
 }
@@ -52,12 +57,12 @@ setup_standbys() {
 
 #############################
 wait_for_healthy_master() {
-	printf "\n-----\nWaiting for master to report healthy...\n"
+	announce_section "Waiting for master to report healthy..."
 	set +e
 	while : ; do
 		printf "..."
 		sleep 2
-		healthy=$(curl -sk https://conjur_master/health | jq -r '.ok')
+		healthy=$(curl -sk $HEALTH_URL | jq -r '.ok')
 		if [[ $healthy == true ]]; then
 			break
 		fi
@@ -68,13 +73,13 @@ wait_for_healthy_master() {
 
 #############################
 wait_for_standbys() {
-	printf "\n-----\nWaiting for all standbys to report streaming replication...\n"
+	announce_section "Waiting for all standbys to report streaming replication..."
 	set +e
 	let num_standbys=$NUM_STATEFUL_NODES-1
 	while : ; do
 		printf "..."
 		sleep 2
-		standby_state=$(curl -sk https://conjur_master/health | jq -r '.database.archive_replication_status.pg_stat_replication | .[].state')
+		standby_state=$(curl -sk $HEALTH_URL | jq -r '.database.archive_replication_status.pg_stat_replication | .[].state')
 		all_good=true
 		standby_count=0
 		for i in $standby_state; do
@@ -94,8 +99,8 @@ wait_for_standbys() {
 
 #############################
 update_load_balancer() {
-	printf "\n-----\nUpdating load balancer configuration...\n"
-	docker cp ../build/haproxy/haproxy.cfg.cluster \
+	announce_section "Updating load balancer configuration..."
+	docker cp ../../build/haproxy/haproxy.cfg.cluster \
 	   $CONJUR_MASTER_INGRESS:/usr/local/etc/haproxy/haproxy.cfg
 	docker restart $CONJUR_MASTER_INGRESS
 }
@@ -111,7 +116,7 @@ setup_cluster_mgr() {
 
 ###########################
 check_conjur_version() {
-        printf "\n-----\nChecking if Conjur version supports failover...\n"
+        announce_section "Checking if Conjur version supports failover..."
         CONJUR_VERSION=$(docker-compose exec cli conjur version | awk -F " " '/Conjur appliance version:/ { print $4 }')
         CONJUR_MAJOR=$(echo $CONJUR_VERSION | awk -F "." '{ print $1 }')
         CONJUR_MINOR=$(echo $CONJUR_VERSION | awk -F "." '{ print $2 }')
@@ -127,14 +132,17 @@ check_conjur_version() {
 
 #############################
 setup_cluster_state() {
-	printf "\n-----\nConfiguring etcd cluster manager and cluster policy...\n"
+	announce_section "Configuring etcd cluster manager and cluster policy..."
 
 	CONT_LIST=$(docker ps -f "label=role=conjur_node" --format {{.Names}})
 	construct_cluster_policy	# build cluster policy file
 
 					# load policy describing cluster
-        docker-compose exec cli conjur authn login -u admin -p Cyberark1
-	docker-compose exec cli conjur policy load --as-group=security_admin /src/cluster/$CLUSTER_POLICY_FILE
+	tmp_pwd=$(pwd)
+	cd ../../
+        docker-compose exec cli conjur authn login -u admin -p $CONJUR_MASTER_PASSWORD
+	docker-compose exec cli conjur policy load --as-group=security_admin /src/scenarios/cluster/$CLUSTER_POLICY_FILE
+	cd $tmp_pwd
 
 	for cname in $CONT_LIST; do
 		docker exec $cname evoke cluster enroll -n $cname $CLUSTER_NAME
