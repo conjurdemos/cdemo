fb46386faf36f1a23fe10312f6e56a7671602762
Benjamin Floyd <dataplex@gmail.com>
2018-02-07 10:02:41 -0600

    Updating summon scenaio

--- scenarios/summon/0-setup-summon.sh
+++ scenarios/summon/0-setup-summon.sh
@@ -1,16 +1,15 @@
 #!/bin/bash 
 set -eo pipefail
 
-CONJUR_MASTER_ORGACCOUNT=dev
-CONJUR_APPLIANCE_URL=https://conjur_follower/api
+. ../../etc/_loadcfg.sh
+
+CONJUR_APPLIANCE_URL=https://$CONJUR_FOLLOWER_INGRESS/api
 CONJUR_CERT_FILE=../../etc/conjur_follower.pem
 CONJUR_CERT_FILE_ON_HOST=/etc/conjur_follower.pem
 HOST_IMAGE=rack-vm:1.0
 CNAME=cdemo_vm_1
-HOSTNAME=webapp1/tomcat_host
-
 				# start container - summon is already installed
-docker-compose up -d vm 
+docker-compose -f ../../docker-compose.yml up -d vm 
 				# copy over conjur.conf
 cat <<CONF_EOF | docker exec -i $CNAME sudo tee /etc/conjur.conf
 ---
@@ -24,12 +23,12 @@ docker cp $CONJUR_CERT_FILE $CNAME:$CONJUR_CERT_FILE_ON_HOST
 
 				# generate api key
 docker exec conjur_cli conjur authn login -u bob -p foo
-api_key=$(docker exec conjur_cli conjur host rotate_api_key --host $HOSTNAME)
+api_key=$(docker exec conjur_cli conjur host rotate_api_key --host $APP_HOSTNAME)
 
 				# copy over identity file
 cat <<IDENTITY_EOF | docker exec -i $CNAME sudo tee /etc/conjur.identity
 machine $CONJUR_APPLIANCE_URL
-    login host/$HOSTNAME
+    login host/$APP_HOSTNAME
     password $api_key
 IDENTITY_EOF
 
