8d0cb4c55af68772a94b35798e0a29f2c9e4375f
Benjamin Floyd <dataplex@gmail.com>
2018-02-07 09:37:05 -0600

    Fixing splunk scenario

--- scenarios/splunk/0-setup-splunk.sh
+++ scenarios/splunk/0-setup-splunk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 set -o pipefail
-docker-compose up -d splunk
+docker-compose -f ../../docker-compose.yml up -d splunk
 printf "\nWatching Splunk log. Ctrl-C when you see the following:\n"
 printf "splunk_1  | Listening for data on TCP port 1514.\n"
 read -p "Press enter to continue..."
