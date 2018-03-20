cb42ee6e555b559ad7583b9b8fb9b07c57a11363
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-03-20 08:44:45 -0600

    new work

---
+++ future/ansible-conjur-identity-example/0_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+
+function finish {
+  echo 'Removing docker environment'
+  echo '---'
+  docker-compose down -v
+}
+trap finish EXIT
+
+function main {
+  docker-compose up -d conjur-master
+  docker-compose logs -f conjur-master
+}
+
+function foo {
+  echo 'foo'
+}
+
+main
