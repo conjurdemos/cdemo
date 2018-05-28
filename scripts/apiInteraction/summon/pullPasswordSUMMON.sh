54c9208eeed8856d7ef16db222b35be54e313ebc
Darren Khan <djkhan85@gmail.com>
2018-05-28 13:38:49 -0400

    cleaned up line spacing

--- scripts/apiInteraction/summon/pullPasswordSUMMON.sh
+++ scripts/apiInteraction/summon/pullPasswordSUMMON.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-source ../utils.sh
-
 main(){
-  printf '\n-----'
-  printf '\nThis script will pull a password using summon.\n'
-  summon_secret
-  printf '\n-----\n'
+  if [ "$1" == "automate" ];
+    then
+      summon_jenkins
+  else
+    printf '\n-----'
+    printf '\nThis script will pull a password using summon.\n'
+    summon_interactive
+    printf '\n-----\n'
+  fi
 }
 
-summon_secret(){
+summon_interactive(){
   local api=$(cat ~/.netrc | grep password | awk '{print $2}')
   local account=$(cat ~/.conjurrc | grep account | awk '{print $2}')
   local login=$(cat ~/.netrc | grep login | awk '{print $2}')
@@ -20,6 +23,16 @@ summon_secret(){
   printf "\n"
   pause 'Press [ENTER] key to continue...'
   printf "\nGrabbing secret aws_access_key: $aws_secret"
-
 }
-main
+
+summon_jenkins(){
+  local api=$(cat ~/.netrc | grep password | awk '{print $2}')
+  local account=$(cat ~/.conjurrc | grep account | awk '{print $2}')
+  local login=$(cat ~/.netrc | grep login | awk '{print $2}')
+  
+  printf "Using Conjur login name: $login\n"
+  printf "Using api: $api\n"
+  printf "Using account: $account"
+  printf "\nGrabbing secret aws_access_key: $aws_secret"  
+}
+main $1
