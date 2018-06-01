d107db18147138dee514b86c58e0ca1e32ced709
Darren Khan <djkhan85@gmail.com>
2018-06-01 00:07:33 -0400

    removed interactive script components as they are not needed with summon

--- conjurDemo/roles/gitlabConfig/templates/JOB1_Summon/pullPasswordSUMMON.sh
+++ conjurDemo/roles/gitlabConfig/templates/JOB1_Summon/pullPasswordSUMMON.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
 main(){
-  if [ "$1" == "automate" ];
-    then
-      summon_jenkins
-  else
-    printf '\n-----'
-    printf '\nThis script will pull a password using summon.\n'
-    summon_interactive
-    printf '\n-----\n'
-  fi
-}
-
-summon_interactive(){
-  local api=$(cat ~/.netrc | grep password | awk '{print $2}')
-  local account=$(cat ~/.conjurrc | grep account | awk '{print $2}')
-  local login=$(cat ~/.netrc | grep login | awk '{print $2}')
-  
-  printf "Using Conjur login name: $login\n"
-  printf "Using api: $api\n"
-  printf "Using account: $account"
-  printf "\n"
-  pause 'Press [ENTER] key to continue...'
-  printf "\nGrabbing secret aws_access_key: $aws_secret"
+ summon_jenkins
 }
 
 summon_jenkins(){
