88f196a30c8539547b95ab7f0b4f1de078338d56
Ryan Prior <ryanprior@gmail.com>
2018-09-11 17:54:39 -0500

    Adds script to check network connectivity before installation

--- installAnsible.sh
+++ installAnsible.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-main(){
-  printf '\n-----'
-  printf '\nInstalling dependencies'
-  if [[ $(cat /etc/*-release | grep -w ID_LIKE) == 'ID_LIKE="rhel fedora"' ]]; then
+set -eu
+
+function separator() {
+    echo "-----"
+}
+
+function install_ansible_yum() {
+    echo "Updating yum"
+    sudo yum update -y &> /dev/null
+    echo "Upgrading system"
+    sudo yum upgrade -y &> /dev/null
+    echo "Installing EPEL repo"
+    sudo yum install epel-release -y &> /dev/null
+    echo "Updating Repolist"
+    sudo yum repolist &> /dev/null
+    sudo yum update -y &> /dev/null
+    echo "Installing Ansible"
+    sudo yum install ansible -y &> /dev/null
+}
+
+function install_ansible_apt() {
+    echo "Installing dirmngr"
+    sudo apt-get install dirmngr
+    echo "Installing source repo"
+    echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
+    echo "Installing keyserver"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    echo "Updating APT"
+    sudo apt-get update -y
+    echo "Installing Ansible"
+    sudo apt-get install ansible -y
+}
+
+bin/check-network
+separator
+echo 'Installing dependencies'
+if [[ $(cat /etc/*-release | grep -w ID_LIKE) == 'ID_LIKE="rhel fedora"' ]]; then
     install_ansible_yum
-  elif [[ $(cat /etc/*-release | grep -w ID) == 'ID=debian' ]]; then
+elif [[ $(cat /etc/*-release | grep -w ID) == 'ID=debian' ]]; then
     install_ansible_apt
-  else
-    printf "\nCouldn't figure out OS"
-  fi
-  printf '\n-----\n'
-}
-
-install_ansible_yum(){
-  printf '\nUpdating yum\n'
-  sudo yum update -y &> /dev/null
-  printf '\nUpgrading system\n'
-  sudo yum upgrade -y &> /dev/null
-  printf '\nInstalling EPEL repo\n'
-  sudo yum install epel-release -y &> /dev/null
-  printf '\nUpdating Repolist\n'
-  sudo yum repolist &> /dev/null
-  sudo yum update -y &> /dev/null
-  printf '\nInstalling Ansible\n'
-  sudo yum install ansible -y &> /dev/null
-}
-
-install_ansible_apt(){
-  printf "\nInstalling dirmngr"
-  sudo apt-get install dirmngr
-  printf "\nInstalling source repo"
-  echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
-  printf "\nInstalling keyserver"
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  printf "\nUpdating APT"
-  sudo apt-get update -y
-  printf "\nInstalling Ansible"
-  sudo apt-get install ansible -y
-}
-main
+else
+    echo "This script cannot install Ansible automatically"
+    echo "on your operating system. Please figure out how to"
+    echo "do so, then submit a patch to add support."
+    echo "Thank you!"
+    echo "-maintainer"
+    echo "https://github.com/conjurdemos/cdemo/blob/master/installAnsible.sh"
+fi
+separator
+printf "\n"
