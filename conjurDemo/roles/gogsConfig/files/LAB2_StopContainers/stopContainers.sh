6f15887924c12487565f6f7a7f01089e68d5aa74
Darren Khan <djkhan85@gmail.com>
2019-03-18 11:45:02 -0400

    Updated lab2's `stopContainers.sh` to remove stopped containers
    Previously, stopped containers would remain dangling.

--- conjurDemo/roles/gogsConfig/files/LAB2_StopContainers/stopContainers.sh
+++ conjurDemo/roles/gogsConfig/files/LAB2_StopContainers/stopContainers.sh
@@ -2,7 +2,7 @@
 
 main (){  
   echo " Stopping $1 containers"
-  docker container rm -f $(docker container ls -f name=$1* -q)
+  docker container rm -f $(docker container ls -f name=$1* -aq)
 }
 
 main $1
